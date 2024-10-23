6a9e8fa4c97368bdaca18c5eeb0689c1ec4e3ed5
Mulyana N <mulyanan.ur26@gmail.com>
2024-10-23 13:31:28 +0700

    Add Navigation Links

--- src/Foot.jsx
+++ src/Foot.jsx
@@ -1,18 +1,22 @@
+import Logo from "./img/LinkedIn_logo.png";
+
 const FooterFunction = () => {
   const name = "Mulyana";
   const year = new Date().getFullYear();
   return (
     // <div class="d-flex flex-column min-vh-100">
-      <footer className="p-3 bg-dark text-white mt-5 ">
-        <div className="container">
-          <p className="mb-1 text-center">{`Copyright © ${year}  ${name}. All Rights Reserved`}</p>
-          <p className="mb-0 text-center">
-            <a href="https://www.linkedin.com/in/mulyana-nurcahyani-45216b267/">
-              @Mulyana N
-            </a>
-          </p>
-        </div>
-      </footer>
+    <footer className="p-3 bg-dark text-white mt-5 ">
+      <div className="container">
+        <p className="mb-1 text-center">{`Copyright © ${year}  ${name}. All Rights Reserved`}</p>
+        <p className="mb-0 text-center">
+          <a href="https://www.linkedin.com/in/mulyana-nurcahyani-45216b267/">
+            Linkedin
+          </a>
+          &ensp;
+          <a href="https://www.instagram.com/mulyanan_/">Instagram</a>
+        </p>
+      </div>
+    </footer>
     // </  div>
   );
 };
