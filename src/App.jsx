a5d9fb37e2b4d7881e13a347a3f79ad84e9a20e0
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-05 17:33:36 +0700

    Update move router from main to app using Routers.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,10 @@
-import { useState } from "react";
-import { AddBookForm } from "./Page/Component/BooksForm";
-import { BookTable } from "./Page/Component/BooksTable";
-import { FooterFunction } from "./Foot";
-import { HeaderFunction } from "./Head";
-// import BooksPage from "./Page/BooksPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routers";
 
 function App() {
-  const [isEditing, setIsEditing] = useState(false);
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [book, setBook] = useState([]);
-  const router = createBrowserRouter([
-    {
-      path: "/books",
-      element: (
-        <BookTable
-          book={book}
-          setBook={setBook}
-          setIsEditing={setIsEditing}
-          setSelectedBook={setSelectedBook}
-        />
-      ),
-      errorElement: <div>404 Not Found</div>,
-    },
-    {
-      path: "/books/add",
-      element: (
-        <AddBookForm
-          book={book}
-          setBook={setBook}
-          isEditing={isEditing}
-          setIsEditing={setIsEditing}
-          selectedBook={selectedBook}
-        />
-      ),
-    },
-  ]);
-
   return (
     <>
-      <HeaderFunction />
       <RouterProvider router={router} />
-      <FooterFunction />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainPage from "./Page/MainPage";
-import BooksPage from "./Page/BooksPage";
-import { BookTable } from "./Component/Books/BooksTable";
-import { AddBookForm } from "./Component/Books/BooksForm";
-import LandingPage from "./Page/LandingPage";
-import MembersPage from "./Page/MembersPage";
-import MemberTable from "./Component/Members/MemberTable";
-import MemberForm from "./Component/Members/MemberForm";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-    // errorElement: <div>404 Not Found</div>,
-    children: [
-      { path: "", element: <MainPage /> },
-      {
-        path: "/books",
-        element: <BooksPage />,
-        children: [
-          { path: "", element: <BookTable /> },
-          { path: "/books/add", element: <AddBookForm /> },
-          { path: "/books/:id", element: <AddBookForm /> },
-        ],
-      },
-      {
-        path: "/members",
-        element: <MembersPage />,
-        children: [
-          { path: "", element: <MemberTable /> },
-          { path: "/members/add", element: <MemberForm /> },
-          { path: "/members/:id", element: <MemberForm /> },
-        ],
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-
-    {/* <App/> */}
+    <App />
   </React.StrictMode>
 );
