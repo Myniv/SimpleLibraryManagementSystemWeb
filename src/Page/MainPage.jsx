c388c6fb0c1f9806113db45fcd3e92ce54078b70
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-08 15:21:27 +0700

    Add Statistic to mainPage

--- src/Page/MainPage.jsx
+++ src/Page/MainPage.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+
 const MainPage = () => {
-    return(
-        <>
-        <h1>This is Main Page</h1>
-        </>
-    )
-}
-export default MainPage
+  const [member, setMember] = useState([]);
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const storedMember = JSON.parse(localStorage.getItem("member")) || [];
+    setMember(storedMember);
+
+    const storedBook = JSON.parse(localStorage.getItem("book")) || [];
+    setBook(storedBook)
+  },[]);
+
+  return (
+    <div className="container text-center my-4">
+      <h2>Library Dashboard</h2>
+      <div className="container d-flex justify-content-center">
+        <div className="card text-center m-3" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title bg-dark text-white p-2 rounded">
+              Total Books
+            </h5>
+            <p
+              className="card-text"
+              style={{ fontSize: "2rem", fontWeight: "bold" }}
+            >
+              {book.length}
+            </p>
+          </div>
+        </div>
+
+        <div className="card text-center m-3" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title bg-dark text-white p-2 rounded">
+              Total Members
+            </h5>
+            <p
+              className="card-text"
+              style={{ fontSize: "2rem", fontWeight: "bold" }}
+            >
+              {member.length}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MainPage;
