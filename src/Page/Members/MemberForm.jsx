0fbba32eafcdf277a243b3d2aea3a79c25403687
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-15 04:42:06 +0700

    Update eslint disable at memberform

--- src/Page/Members/MemberForm.jsx
+++ src/Page/Members/MemberForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from "react";
 import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import LoadingAddEdit from "../../Component/Elements/LoadingAddEdit";
