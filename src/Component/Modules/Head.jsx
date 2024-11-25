1b6e235b8f9b3341365d457fae7e17343f6492c2
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-25 23:42:19 +0700

    Update header for adding the navigation and filter the navbar using roles

--- src/Component/Modules/Head.jsx
+++ src/Component/Modules/Head.jsx
@@ -1,7 +1,82 @@
-import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { logout } from "../../Redux/authSlice";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const HeaderFunction = () => {
-  // const currentDate = new Date().toDateString();
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const menuItems = [
+    {
+      label: "Dashboard",
+      path: "/",
+      visibleForAll: true,
+    },
+    {
+      label: "Profile",
+      path: "/profile",
+      visibleForRoles: ["Librarian", "Library Manager", "Library User"],
+    },
+    {
+      label: "Book List",
+      path: "/books",
+      visibleForRoles: ["Librarian", "Library Manager"],
+    },
+    {
+      label: "Users",
+      path: "/members",
+      visibleForRoles: ["Library Manager"],
+    },
+    {
+      label: "Login",
+      path: "/login",
+      isAuthenticated: false,
+    },
+    {
+      label: "Register",
+      path: "/register",
+      isAuthenticated: false,
+    },
+    {
+      label: "logout",
+      path: "/logout",
+      isAuthenticated: true,
+    },
+  ];
+
+  const isMenuVisible = (item) => {
+    //for showing all menu to user
+    if (item.visibleForAll) {
+      return true;
+    }
+
+    //for showing pages if are not login yer
+    if (item.isAuthenticated === false && !currentUser) {
+      return true;
+    }
+
+    //for showing logout if already login
+    if (item.label === "logout" && currentUser) {
+      return true;
+    }
+
+    //for role for spesifict menu
+    if (item.visibleForRoles && currentUser?.role) {
+      return item.visibleForRoles.some((role) =>
+        currentUser.role.includes(role)
+      );
+    }
+
+    return false;
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
   return (
     <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-dark text-white">
       <div className="container">
@@ -10,60 +85,30 @@ const HeaderFunction = () => {
           <h1 className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             Library Management
           </h1>
-          {/* <p className="text-end">{currentDate}</p> */}
-          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 nav nav-pills">
-            <li className="nav-item">
-              <NavLink
-                to="/"
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-              >
-                Menu
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/searchbooks"
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-              >
-                Search Books
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink
-                to="/books"
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-              >
-                Books
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/members"
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-              >
-                Members
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/transactions"
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-              >
-                Borrow
-              </NavLink>
-            </li>
-          </ul>
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="#">Library App</Navbar.Brand>
+              <Nav className="me-auto">
+                {menuItems.filter(isMenuVisible).map((item, index) => (
+                  <NavLink
+                    key={index}
+                    to={item.path}
+                    onClick={item.label === "logout" ? handleLogout : null}
+                  >
+                    {item.label}
+                  </NavLink>
+                ))}
+              </Nav>
+            </Container>
+          </Navbar>
+          {currentUser && (
+            <hd>
+              Welcome,{" "}
+              <strong>
+                {currentUser.user?.userName ? currentUser.user.userName : currentUser.role}
+              </strong>
+            </hd>
+          )}
         </div>
       </div>
     </header>
@@ -71,3 +116,59 @@ const HeaderFunction = () => {
 };
 
 export { HeaderFunction };
+
+{
+  /* <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 nav nav-pills">
+  <li className="nav-item">
+    <NavLink
+      to="/"
+      className={({ isActive }) => {
+        return isActive ? "nav-link active" : "nav-link";
+      }}
+    >
+      Menu
+    </NavLink>
+  </li>
+  <li className="nav-item">
+    <NavLink
+      to="/searchbooks"
+      className={({ isActive }) => {
+        return isActive ? "nav-link active" : "nav-link";
+      }}
+    >
+      Search Books
+    </NavLink>
+  </li>
+
+  <li className="nav-item">
+    <NavLink
+      to="/books"
+      className={({ isActive }) => {
+        return isActive ? "nav-link active" : "nav-link";
+      }}
+    >
+      Books
+    </NavLink>
+  </li>
+  <li className="nav-item">
+    <NavLink
+      to="/members"
+      className={({ isActive }) => {
+        return isActive ? "nav-link active" : "nav-link";
+      }}
+    >
+      Members
+    </NavLink>
+  </li>
+  <li className="nav-item">
+    <NavLink
+      to="/transactions"
+      className={({ isActive }) => {
+        return isActive ? "nav-link active" : "nav-link";
+      }}
+    >
+      Borrow
+    </NavLink>
+  </li>
+</ul> */
+}
