e3e43a86ff4e7d7e6ddb5be1c93364aa33d8b8ed
Mulyana N <mulyanan.ur26@gmail.com>
2024-12-04 10:50:09 +0700

    Update profile

--- src/Page/Profile.jsx
+++ src/Page/Profile.jsx
@@ -1,3 +1,4 @@
+import { Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +8,6 @@ const Profile = () => {
   const { user: currentUser } = useSelector((state) => state.auth);
   console.log("Current User:", currentUser);
 
-
   if (!currentUser) {
     navigate("/login");
     return null;
@@ -15,14 +15,62 @@ const Profile = () => {
 
   return (
     <div className="container">
-      <p>
-        <strong>Email</strong> {currentUser?.email || "No Email"}
-      </p>
-      <strong>Roles: {currentUser?.role}</strong>
-      <ul>
-        {currentUser.roles &&
-          currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-      </ul>
+      <div className="d-flex justify-content-center align-items-center mb-4">
+        <div className="d-flex align-items-center justify-content-center">
+          <img
+            src="/img/boyIcon.png"
+            alt="Profile"
+            className="rounded-circle me-3"
+            width="100"
+            height="100"
+          />
+          <div>
+            <h3 className="m-0">
+              {currentUser.user.fName} {currentUser.user.lName}
+            </h3>
+
+            <p className="text-muted m-0">
+              Position:{" "}
+              {currentUser.user.userPosition
+                ? currentUser.user.userPosition
+                : "N/A"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <Card>
+        <Card.Body>
+          <h5>Personal Information</h5>
+          <div className="mb-3">
+            <strong>Personal Data</strong>
+            <p>Privilage: {currentUser.user.userPrivilage}</p>
+            <p>
+              Card Number:{" "}
+              {currentUser.user.libraryCardNumber
+                ? currentUser.user.libraryCardNumber
+                : "N/A"}
+            </p>
+          </div>
+          <div className="mb-3">
+            <p>Expired Date: {currentUser.user.libraryCardExpiredDate}</p>
+          </div>
+          <div className="mb-3">
+            <strong>Contact</strong>
+            <p>
+              Primary Email: {currentUser.email ? currentUser.email : "N/A"}
+            </p>
+          </div>
+          <div className="mb-3">
+            <strong>Roles: {currentUser?.role}</strong>
+            <ul>
+              {currentUser.roles &&
+                currentUser.roles.map((role, index) => (
+                  <li key={index}>{role}</li>
+                ))}
+            </ul>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
